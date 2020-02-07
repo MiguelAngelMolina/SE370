2a1bddcea891c96efe8fa23a23a75d0b76d6d638
MiguelAngelMolina <formiguelmolina@gmail.com>
2020-02-07 14:56:44 -0800

    Add files via upload

---
+++ src/main/java/com/example/demo/Se370Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se370Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se370Application.class, args);
+	}
+
+}
